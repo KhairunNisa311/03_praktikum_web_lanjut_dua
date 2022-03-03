6a80062721c70c42dc7463127f913543b13532c9
KhairunNisa311 <ginazoom1@gmail.com>
2022-03-03 21:04:05 +0700

    praktikum 2

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\CompanyController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
+
+Route::get('/', [CompanyController:: class, 'index']);
+
+Route::get('/company', [CompanyController:: class, 'company']);
+
+Route::get('/services', [CompanyController:: class, 'services']);
+
+Route::get('/contact', [CompanyController:: class, 'contact']);
